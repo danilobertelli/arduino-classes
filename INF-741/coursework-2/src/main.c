308a5371cecb0d7414fa6fed6b6fce823d185303
Danilo Arthur Bertelli <vntdabe@venturus.org.br>
2016-11-28 12:53:59 -0200

    First version of course work 2

--- INF-741/coursework-2/src/main.c
+++ INF-741/coursework-2/src/main.c
@@ -5,18 +5,25 @@
 #include <avr/io.h>
 #include <uart.h>
 
-#define IDLE_MODE 0
-#define POWER_DOWN_MODE 1
-#define POWER_SAVE_MODE 2
-#define STANDBY_MODE 3
-#define EXT_STANDBY_MODE 4
+#define MAX_PLAYERS 9
+#define MAX_NOTES 100
+#define DEBUG 1 // set 0 to disable debug prints
 
 volatile long interruptions = 0;  // Controls how much interruptions has fired
 volatile long bounceTime = 0;     // debounce control
-volatile long sleepTime = 0;      // Time before sleeping
-volatile long totalSleepTime = 0; // Total Time sleeping
-volatile long delayTime = 0;
-volatile bool shouldSleep = true;
+volatile long bounceTime2 = 0;     // debounce control
+
+// Players array
+volatile int players[MAX_PLAYERS]; // number of players
+volatile int nro_players = 0;
+volatile long player_time = 0;
+volatile char notes[MAX_NOTES]; // notes sequence array
+volatile int N = 0;
+volatile int round = 0;
+volatile int current_player = -1;
+volatile int current_player_note = 0;
+
+void round_reset();
 
 long get_millis() {
     // First, we need to count how much interruptions we had and multiply it by 16384us
@@ -26,43 +33,128 @@ long get_millis() {
     return micros / 1000;
 }
 
-void sleep_cpu() {
-    SMCR |= (1<<SE);
-    sleepTime = get_millis(); // Get current time before goes to sleep
-    asm("sleep");
-}
-
-void set_sleep_mode(char mode) {
-    switch (mode) {
-        case IDLE_MODE:
-           SMCR &= ~(1 << SM0);
-           SMCR &= ~(1 << SM1);
-           SMCR &= ~(1 << SM2);
-        break;
-        case POWER_DOWN_MODE:
-           SMCR &= ~(1 << SM0);
-           SMCR |= (1<<SM1);
-        break;
-        case POWER_SAVE_MODE:
-           SMCR |= (1<<SM1) | (1<<SM0);
-        break;
-        case STANDBY_MODE:
-           SMCR &= ~(1 << SM0);
-           SMCR |= (1<<SM2) | (1<<SM1);
-        break;
-        case EXT_STANDBY_MODE:
-           SMCR |= (1<<SM2) | (1<<SM1) | (1<<SM0);
-        break;
+// Resets all CURRENT players to 0 so they can play next turn
+void reset_players() {
+    // before any new round, check players status
+    if (DEBUG == 1) {
+        printf("\nLast player, reset! \n");
+    }
+    for (int i = 0; i < nro_players; i++) {
+        if(players[i] == 1) {
+            players[i] = 0;
+        }
     }
 }
 
-// It increments the total time that CPU was sleeping
-void increment_sleep_time(long timeInMs) {
-    if (sleepTime > 0) {
-        long totalTimeSleeping = timeInMs - sleepTime;
-        totalSleepTime = totalTimeSleeping + totalSleepTime;
+// Returns how many players do not played yet this turn
+int get_not_played_players() {
+    int n_played = 0;
+    for (int i = 0; i < nro_players; i++) {
+        if(players[i] == 0) {
+            n_played++;
+        }
     }
-    sleepTime = 0; // reset value to wait next bed time
+    return n_played;
+}
+
+// Returns how many players are still on the game
+int get_active_players() {
+    int active = 0;
+    for (int i = 0; i < nro_players; i++) {
+        if(players[i] != -1) {
+            active++;
+        }
+    }
+    return active;
+}
+
+// Return who won the game
+int find_winner() {
+    int winner = 0;
+    for (int i = 0; i < nro_players; i++) {
+        if(players[i] != -1) {
+            winner = i + 1;
+            break;
+        }
+    }
+    return winner;
+}
+
+void round_turn() {
+    // find next player
+    for (int i = 0; i < nro_players; i++) {
+        if(players[i] == 0) {
+            players[i] = 1; // mark as played
+            current_player = i;
+            break;
+        }
+    }
+
+    printf("Player %d\n", current_player + 1);
+    current_player_note = 0;
+    player_time = get_millis();
+}
+
+// Prepare for next round
+void round_start() {
+    int active = get_active_players();
+
+    if (active == 1) {
+        // we already have a winner
+        printf("\nGame over! Player %d won\n",  find_winner());
+        round_reset();
+        return;
+    } else {
+        // Continue gameplay
+        if (get_not_played_players() == 0) {
+            // there´s no player waiting in line, clear flag
+            reset_players();
+        }
+
+        // increments round
+        round++;
+        printf("\nROUND %d\n", round);
+
+        round_turn();
+    }
+}
+
+// it resets the system
+void round_reset() {
+    round = 0;
+    N = 0;
+    current_player = -1;
+    current_player_note = 0;
+    nro_players = 0;
+    interruptions = 0; // clear timer 
+
+    // reset players
+    for (int i = 0; i < MAX_PLAYERS; i++) {
+        players[i] = -1;
+    }
+    // reset notes list
+    for (int i = 0; i < MAX_NOTES; i++) {
+        notes[i] = '0';
+    }
+    printf("\nWelcome to the follow the leader game! Please enter the number of players: [2-9]\n");
+    int count = 0;
+    scanf("%d", &count);
+    while (count < 2 || count > 9) {
+        printf("Enter a valid number of players\n");
+        scanf("%d", &count);
+    }
+
+    if (DEBUG == 1) {
+        printf("Number of players: %d\n", count);
+    }
+
+    // active count players
+    for (int i = 0; i < count; i++) {
+        players[i] = 0;
+    }
+
+    nro_players = count;
+    round_start();
 }
 
 int main (void) {
@@ -75,13 +167,13 @@ int main (void) {
     DDRD  &= ~(1 << DDD2);    // PD2 (PCINT0 pin) is now an input
     PORTD |=  (1 << PORTD2);  // PD2 is now with pull-up enabled
     EICRA |= (1 << ISC00);
-    EIMSK |=  (1 << INT0);    // Allow INT0 interrupts
+    EIMSK |=  (1 << INT0);
 
     // Configure button interruptions 3
     DDRD  &= ~(1 << DDD3);    // PD3 (PCINT0 pin) is now an input
     PORTD |=  (1 << PORTD3);  // PD3 is now with pull-up enabled
     EICRA |= (1 << ISC10);
-    EIMSK |=  (1 << INT1);    // Allow INT0 interrupts
+    EIMSK |=  (1 << INT1);
 
     // Don't know why but the led is starting ON so turn it OFF as a workaround
     DDRB = 0xFF;       // Configure PORTB as output
@@ -97,21 +189,67 @@ int main (void) {
 
     sei(); // Enable Global Interrupts
 
+    round_reset();
     while(1) {
-        if (shouldSleep) {
-            // set_sleep_mode(POWER_SAVE_MODE);
-            // sleep_cpu();
+        // bla
+    }
+}
+
+// Handle button1 interruption (C notes)
+ISR (INT0_vect) {
+    long timeNow = get_millis();
+
+    if (bounceTime == 0) {
+        bounceTime = timeNow;
+        if (DEBUG == 1) {
+            printf("C");
+        }
+
+        if (current_player_note == N) {
+            notes[N] = 'C';
+            N++;
+            current_player_note++;
+            round_start();
+        } else if (current_player_note < N) {
+            if (notes[current_player_note] == 'C') {
+                current_player_note++;
+            } else {
+                // Player missed the note.. game over for him
+                players[current_player] = -1;
+                printf("Incorrect sequence! You have been eliminated.\n");
+                round_start();
+            }
         }
     }
 }
 
-// Handle button interruption
-ISR (INT0_vect) {
-    printf("INT0:");
-}
-
+// Handle button2 interruption (F notes)
 ISR (INT1_vect) {
-    printf("INT1:");
+    long timeNow = get_millis();
+
+    if (bounceTime2 == 0) {
+        player_time = timeNow;
+        bounceTime2 = timeNow;
+        if (DEBUG == 1) {
+            printf("F");
+        }
+
+        if (current_player_note == N) {
+            notes[N] = 'F';
+            N++;
+            current_player_note++;
+            round_start();
+        } else if (current_player_note < N) {
+            if (notes[current_player_note] == 'F') {
+                current_player_note++;
+            } else {
+                // Player missed the note.. game over for him
+                players[current_player] = -1;
+                printf("Incorrect sequence! You have been eliminated.\n");
+                round_start();
+            }
+        }
+    }
 }
 
 ISR(TIMER2_OVF_vect) {
@@ -119,16 +257,22 @@ ISR(TIMER2_OVF_vect) {
     interruptions++;
 
     long timeNow = ((interruptions * 16384) + (TCNT2 * 64)) / 1000;
-    increment_sleep_time(timeNow); // as the interruption woke up the CPU, get time sleeping
 
-    // handle bounce control
-    if (bounceTime > 0 && timeNow - bounceTime > 100) {
+    // handle bounce control on button 1
+    if (bounceTime > 0 && timeNow - bounceTime > 300) {
         bounceTime = 0;
     }
 
-    // handle delay to sleep after print
-    if (delayTime > 0 && timeNow - delayTime > 30) {
-        delayTime = 0;
-        shouldSleep = true;
+    // handle bounce control on button 2
+    if (bounceTime2 > 0 && timeNow - bounceTime2 > 300) {
+        bounceTime2 = 0;
+    }
+
+    // handle player timeout
+    if (player_time > 0 && timeNow - player_time > 5000) {
+        player_time = 0;
+        players[current_player] = -1;
+        printf("Time expired. You have been eliminated!\n");
+        round_start();
     }
 }
